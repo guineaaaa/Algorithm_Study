c20ac9ae1bd4ce940756aebc142ce36b5d2c0692
guinea <165776804+guineaaaa@users.noreply.github.com>
2025-01-05 16:11:14 +0900

    [Silver II] Title: 스택 수열, Time: 856 ms, Memory: 112348 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -5,49 +5,48 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc=new Scanner(System.in);
-
-        int N=sc.nextInt();
-        int A[]=new int[N];
-
-        for(int i=0;i<A.length;i++) {
-            A[i]=sc.nextInt();
-        }
-
-        Stack<Integer> stack=new Stack<>();
-        StringBuffer bf=new StringBuffer();
-
-        int num=1; boolean result=true;
-
-        for(int i=0;i<A.length;i++) {
-            int n=A[i];
-
-            if(n>=num) {
-                while(n>=num) {
-                    stack.push(num++);
-                    bf.append("+\n");
-                }
-                stack.pop();
-                bf.append("-\n");
-            }
-            else {
-                int p=stack.pop();
-
-                // 스택의 가장 위 수가 만들어야 하는 수열의 수 보다 크다면 수열 출력 불가
-                if(p>n) {
-                    result=false;
-                    System.out.println("NO");
-                    break;
-                }
-                else {
-                    bf.append("-\n");
-                }
-            }
-        }
-
-        if(result) {
-        System.out.println(bf.toString());
-        }
+    	Scanner sc=new Scanner(System.in);
+    	
+    	int n=sc.nextInt();
+    	int arr[]=new int[n];
+    	
+    	for(int i=0;i<arr.length;i++) {
+    		arr[i]=sc.nextInt();
+    	}
+    	
+    	// 스택 수열 만들기
+    	Stack<Integer> stack=new Stack<>();
+    	StringBuffer bf=new StringBuffer();
+    	
+    	int num=1; boolean result=true;
+    	
+    	for(int i=0;i<arr.length;i++) {
+    		int a=arr[i];
+    		
+    		if(a>=num) {
+    			while(a>=num) {
+    				stack.push(num++);
+    				bf.append("+\n");
+    			}
+    			stack.pop();
+    			bf.append("-\n");
+    		}else {
+    			int peek=stack.pop();
+    			
+    			if(peek>a) {
+    				System.out.println("NO");
+    				result=false;
+    				break;
+    			}else {
+    				bf.append("-\n");
+    			}
+    		}
+    	}
+    	if(result) {
+    		System.out.println(bf.toString());
+    	}
+    	
+    	
     }
-
+       
 }
